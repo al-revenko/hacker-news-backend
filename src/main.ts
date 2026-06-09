10a79662fa17945d0fcc6459aa8228d5fd746cb2
Alexander Revenko <revenko.a@bk.ru>
2026-06-09 12:47:41 +0700

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 
 bootstrap().catch((e) => console.error(e));
